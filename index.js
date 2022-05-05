0469a4f4502e6e6adfd3d13acd577ac9c94c0f63
Terence Smith <terencesmith1@me.com>
2022-05-05 10:33:17 -0400

    First commit

---
+++ index.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // code here will execute after the document is loaded
+});
+
+const renderMovies = (movieArray) => {
+  movieArray.forEach((element) => {
+    console.log(element);
+  });
+};
