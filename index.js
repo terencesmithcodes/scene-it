2b0939f08d84bd5fb950c5e34bbdb997e7f4d9cb
Terence Smith <terencesmith1@me.com>
2022-05-05 23:13:39 -0400

    Updated js file

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
+let test = document.querySelector("#movies-container");
+const myForm = document.getElementById("search-form");
+
 document.addEventListener("DOMContentLoaded", function () {
   // code here will execute after the document is loaded
-});
-
-const renderMovies = (movieArray) => {
-  movieArray.forEach((element) => {
-    console.log(element);
+  myForm.addEventListener("submit", function (e) {
+    // event listener code goes here
+    e.preventDefault();
+    const renderMovie = (movieArray) => {
+      movieHtmlArray = movieArray.map((currentMovie) => {
+        return `
+                    <div class="col-3 card">
+                    <div class="card-body">
+                      <img class="card-img-top" id="img" src=${currentMovie.Poster} alt="Card image cap">
+                      <h5 class="card-title" id="title">${currentMovie.Title}</h5>
+                      <h5 class="card-title"id="year">${currentMovie.Year}</h5>
+                    </div>
+                    </div>
+                   
+                    `;
+      });
+      return movieHtmlArray.join("");
+    };
+      test.innerHTML = renderMovie(movieData);
   });
-};
+  // console.log(test);
+
+});
+// console.log(renderMovie)
+
